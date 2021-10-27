f28572222cc7c95d952021a4e2e47a0919d0e430
Sayan <sayan.almaz40@gmail.com>
2021-10-27 13:54:37 +0600

    I made the first version of the page Creator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:multi_book/pages/login_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:multi-book-app/pages/profileScreenCreator.dart';
+import 'package:multi-book-app/profileScreenWidget/profile_widget.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home:LoginScreen(),
-  ));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  static final String title = 'User Profile';
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue.shade300,
+        dividerColor: Colors.black,
+      ),
+      title: title,
+      home: creatorProfilePage(),
+    );
+  }
+}
