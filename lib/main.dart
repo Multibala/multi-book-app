76fe5e0540f3a1b74b5ed043f8a2831744698271
Multibala <qmultibala@gmail.com>
2021-10-27 15:31:31 +0600

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:multi_book/pages/login_screen.dart';
-import 'package:multi_book/pages/main_screen.dart';
+import 'package:multi-book-app/pages/login_screen.dart';
+import 'package:multi-book-app/pages/main_screen.dart';
 
 
 void main() {
@@ -17,18 +16,6 @@ void main() {
     },
   ));
 }
-=======
-import 'package:flutter/services.dart';
-import 'package:multi-book-app/pages/profileScreenCreator.dart';
-import 'package:multi-book-app/profileScreenWidget/profile_widget.dart';
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
->>>>>>> f28572222cc7c95d952021a4e2e47a0919d0e430
 
   runApp(MyApp());
 }
