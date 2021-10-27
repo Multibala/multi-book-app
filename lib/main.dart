dd85581cfd5444acff91e54aa1bc25d9c476848e
Multibala <qmultibala@gmail.com>
2021-10-27 13:03:04 +0600

    second-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:multi_book/pages/login_screen.dart';
+import 'package:multi_book/pages/main_screen.dart';
+
 
 void main() {
-  runApp(const MaterialApp(
-    home:LoginScreen(),
+  runApp( MaterialApp(
+    initialRoute: '/',
+    routes:{
+      '/':(context)=>  MainScreen(),
+      '/login':(context)=>  LoginScreen(),
+      '/profile':(context)=>  MainScreen(),
+      '/wallet':(context)=>  MainScreen(),
+      '/create':(context)=>  MainScreen(),
+
+    },
   ));
 }
 
