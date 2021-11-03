1f5781c0e961cbd887fe12c3a1ab79337ba1349e
zhassbala <besimbekovr@gmail.com>
2021-11-03 14:14:15 +0600

    added some db models

--- backend/multi_team_backend/app/models.py
+++ backend/multi_team_backend/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Author:
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    image = models.ImageField()
 
-# Create your models here.
+class Book:
+    name = models.CharField(max_length=300)
+    description = models.CharField(max_length=5000)
+    price = models.FloatField()
+    cover = models.ImageField()
+    author = models.ForeignKey(to=Author, related_name='books')
+
+
+class Review:
+    user = models.ForeignKey(to=User, related_name='reviews')
+    content = models.CharField(max_length=1000)
+    target = models.ForeignKey()
